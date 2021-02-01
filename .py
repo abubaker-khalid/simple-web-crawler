be1cf5e501c1c4181f02803909f46f411bf8584d
abubaker khalid <78301055+abubaker-khalid@users.noreply.github.com>
2021-02-01 08:53:46 -0800

    Update .py

--- .py
+++ .py
@@ -2,11 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 def spider():
-    page = 1
-    url =('https://www.w3schools.com/html/default.asp')
-    source = requests.get(url)
-    soup = BeautifulSoup(source.text)
-    for link in soup.findAll('h3'):
+    url = 'https://www.w3schools.com/html/default.asp'
+    source_code = requests.get(url)
+    source_code2 = source_code.text
+    soup = BeautifulSoup(source_code2)
+    for link in soup.findAll('a'):
+        href = 'https://www.w3schools.com' + link.get('href')
         title = link.string
         print(title)
+        print(href)
+
 spider()
